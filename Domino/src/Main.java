5f0a994376852f09adf6012f28fdb3a90679f0ff
FireRoMe <matthias_ridder@msn.com>
2015-05-29 16:16:37 +0200

    Test Update

--- Domino/src/Main.java
+++ Domino/src/Main.java
@@ -1,9 +1,20 @@
+import java.awt.Color;
+import java.awt.Container;
+
+import javax.swing.JFrame;
+
 
 public class Main
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Hello World!!!!");
+		JFrame frame = new JFrame("TestFenster");
+		Container pane = new Container();
+		
+		pane.setBackground(Color.GREEN);
+		frame.setBounds(400, 300, 800, 600);
+		frame.setContentPane(pane);
+		frame.setVisible(true);
 	}
 
 }
